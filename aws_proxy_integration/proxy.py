38ceddd29753aaf9d3dc81c7dbd22219ab2ac98d
Martin Barber <martinpbarber@gmail.com>
2019-04-08 14:49:02 +0000

    Initial route implementation

--- aws_proxy_integration/proxy.py
+++ aws_proxy_integration/proxy.py
@@ -1,7 +1,6 @@
 """
 aws_proxy_integration.proxy
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
 This module processes requests from API Gateway using Lambda proxy integration.
 """
 
@@ -9,10 +8,24 @@ class Proxy():
     """API Gateway Lambda proxy integration"""
 
     def __init__(self):
-        pass
+        self.routes = {}
 
-    def route(self, path):
+    def route(self, resource):
         """Route Decorator"""
+        def decorator(view_function):
+            self.routes[resource] = view_function
+            return view_function
+
+        return decorator
 
     def __call__(self, event, context):
         """Process API Gateway event"""
+        resource = event.get('resource')
+        if resource is None:
+            raise ValueError('No resource provided')
+
+        view_function = self.routes.get(resource)
+        if view_function is None:
+            raise ValueError('Route not registered: %s' % resource)
+
+        return view_function()
--- tests/test_proxy.py
+++ tests/test_proxy.py
@@ -1,5 +1,40 @@
+import pytest
 from aws_proxy_integration.proxy import Proxy
 
 def test_proxy_init():
     proxy = Proxy()
-    assert proxy 
+    assert proxy
+
+def test_proxy_add_route():
+    proxy = Proxy()
+    @proxy.route('/')
+    def index():
+        pass
+
+    event = {'resource': '/'}
+    context = {}
+    proxy(event, context)
+
+def test_proxy_no_route_provided():
+    proxy = Proxy()
+    @proxy.route('/')
+    def index():
+        pass
+
+    event = {}
+    context = {}
+    with pytest.raises(ValueError) as exception_info:
+        proxy(event, context)
+    assert str(exception_info.value) == 'No resource provided'
+
+def test_proxy_route_not_registered():
+    proxy = Proxy()
+    @proxy.route('/')
+    def index():
+        pass
+
+    event = {'resource': '/wrong'}
+    context = {}
+    with pytest.raises(ValueError) as exception_info:
+        proxy(event, context)
+    assert str(exception_info.value) == 'Route not registered: /wrong'
