5c23755ead4ce2409f932188ce3643527b17f288
Martin Barber <martinpbarber@gmail.com>
2019-04-08 17:59:45 +0000

    Additional Proxy class comments

--- aws_proxy_integration/proxy.py
+++ aws_proxy_integration/proxy.py
@@ -5,7 +5,49 @@ This module processes requests from API Gateway using Lambda proxy integration.
 """
 
 class Proxy():
-    """API Gateway Lambda proxy integration"""
+    """API Gateway Lambda proxy integration
+
+    When using proxy interation the API Gateway maps incoming request
+    information in the event parameter of the lambda function as follows.
+    {
+        "resource":
+            String containing the resource path
+        "path":
+            String containing the URI path
+        "httpMethod":
+            String containing the HTTP method in uppercase
+        "headers":
+            Dictionary of case sensitive HTTP request headers
+        "multiValueHeaders":
+            Dictionary of case sensitive multi-value HTTP request headers
+        "queryStringParameters":
+            Dictionary of case sensitive query string parameters
+        "multiValueQueryStringParameters":
+            Dictionary of case sensitive multi-value query string parameters
+        "pathParameters":
+            Dictionary of case sensitive path parameters
+        "stageVariables":
+            Dictionary of case sensitive API Gateway stage variables
+        "requestContext":
+            Dictionary of case sensitive Request Context information
+        "body":
+            JSON string of the request body
+        "isBase64Encoded":
+            Boolean to indicate if the request body is Base64-encoded
+    }
+
+    The Lambda function response must be in the following format.
+    {
+        "statusCode":
+            String containing a valid HTTP status code
+        "headers": May be omitted
+            Dictionary containing a any API-specific custom headers
+        "body": May be empty string
+            JSON string of the response body, binary body must be Base64-encoded
+        "isBase64Encoded":
+            Boolean to indicate if the response body is Base64-encoded
+    }
+    """
 
     def __init__(self):
         self.routes = {}
