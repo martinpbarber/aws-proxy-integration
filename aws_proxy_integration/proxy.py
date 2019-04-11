771728256e9790c84f6ec913932b08378a83223c
Martin Barber <martinpbarber@gmail.com>
2019-04-11 08:50:50 -0400

    Add response validation

--- aws_proxy_integration/proxy.py
+++ aws_proxy_integration/proxy.py
@@ -3,6 +3,7 @@ aws_proxy_integration.proxy
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~
 This module processes requests from API Gateway using Lambda proxy integration.
 """
+from http import HTTPStatus
 
 class Proxy():
     """API Gateway Lambda proxy integration
@@ -74,7 +75,7 @@ class Proxy():
         if isinstance(response, str):
             return Response(response).to_dict()
 
-        return Response('', status_code='500').to_dict()
+        return Response('', status_code=500).to_dict()
 
 # pylint: disable=too-few-public-methods
 class Response():
@@ -94,14 +95,28 @@ class Response():
     """
 
     def __init__(self, body,
-                 status_code='200', headers=None, is_base_64_encoded=False):
-        self._body = body
-        self._status_code = status_code
+                 status_code=200, headers=None, is_base_64_encoded=False):
+        if isinstance(body, str):
+            self._body = body
+        else:
+            raise ValueError('body must be a string')
+
+        if any(status_code == code.value for code in HTTPStatus):
+            self._status_code = str(status_code)
+        else:
+            raise ValueError('status_code must be a valid HTTP status code')
+
         if headers is None:
             self._headers = {}
-        else:
+        elif isinstance(headers, dict):
             self._headers = headers
-        self._is_base_64_encoded = is_base_64_encoded
+        else:
+            raise ValueError('headers must be a dictionary')
+
+        if isinstance(is_base_64_encoded, bool):
+            self._is_base_64_encoded = is_base_64_encoded
+        else:
+            raise ValueError('is_base_64_encoded must be a Boolean')
 
     def to_dict(self):
         """Return Response object as a dictionary"""
